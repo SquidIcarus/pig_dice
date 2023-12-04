b3efb5a75464a3f25d01fe1f2863effca9dcb2c8
Landon Isaiah <landonisaiah@gmail.com>
2023-12-04 09:24:02 -0800

    Add getScore funtion for dice roll function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+function PigDice(player, score) {
+    this.player = player;
+    this.score = score
+    this.results = [];
+}
+
+const player1 = new PigDice("player1", 0);
+const player2 = new PigDice("player2", 0);
+console.log(player1)
+
 function getRandomNum() {
     return Math.floor(Math.random() * 6) + 1;
 }
